49a77e665922263a5040b75a0c0c9c632528b0dc
Eiriknil <eirik.nilsen@unit.no>
2020-06-18 13:10:54 +0200

    async calls to get Bearer tokens

--- cypress/integration/menu/menu.js
+++ cypress/integration/menu/menu.js
@@ -1,21 +1,39 @@
-import { Given, When, Then, Before } from "cypress-cucumber-preprocessor/steps"
+import { Given, When, Then, Before } from 'cypress-cucumber-preprocessor/steps';
 import { v4 as uuidv4 } from 'uuid';
 
 const USER_NAME = 'testUser@unit.no';
 
-Before(async () => {
-  cy.addUser(USER_NAME).as('authResult');
+Before(() => {
+  cy.addUser(USER_NAME).then((authResult) => {
+    cy.wrap(authResult).as('authResult');
+  });
 });
 
 Given('that the user is not logged in', () => {
-  cy.visit('/');
-  cy.get('[data-testid=menu-login-button]').should('be.visible');
+  cy.get('@authResult').then((authResult) => {
+    const auth = `Bearer ${authResult.AccessToken}`;
+    cy.visit({
+      url: '/',
+      headers: {
+        Authorization: auth,
+      },
+    });
+    cy.get('[data-testid=menu-login-button]').should('be.visible');
+  });
 });
 
 When('they look at any page in NVA', () => {
-  const uuid = uuidv4();
-  cy.visit(`/${uuid}`);
+  cy.get('@authResult').then((authResult) => {
+    const uuid = uuidv4();
+    const auth = `Bearer ${authResult.AccessToken}`;
+    cy.visit({
+      url: `/${uuid}`,
+      headers: {
+        Authorization: auth,
+      },
+    });
+  });
 });
 Then('they see the Log in button', () => {
   cy.get('[data-testid=menu-login-button]').should('be.visible');
-}); 
+});
