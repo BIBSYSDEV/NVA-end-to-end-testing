6de907a5e9db2317c217096398dc4fd297c3c10c
Eiriknil <eirik.nilsen@unit.no>
2020-06-19 08:29:12 +0200

    Added reject to promise

--- cypress/integration/menu/menu.js
+++ cypress/integration/menu/menu.js
@@ -4,36 +4,23 @@ import { v4 as uuidv4 } from 'uuid';
 const USER_NAME = 'testUser@unit.no';
 
 Before(() => {
-  cy.addUser(USER_NAME).then((authResult) => {
-    cy.wrap(authResult).as('authResult');
-  });
+  cy.addUser(USER_NAME)
+    .then((authResult) => {
+      cy.wrap(authResult).as('authResult');
+    })
+    .catch((err) => console.log(err));
 });
 
 Given('that the user is not logged in', () => {
-  cy.get('@authResult').then((authResult) => {
-    const auth = `Bearer ${authResult.AccessToken}`;
-    cy.visit({
-      url: '/',
-      headers: {
-        Authorization: auth,
-      },
-    });
-    cy.get('[data-testid=menu-login-button]').should('be.visible');
-  });
+  cy.visit('/');
+  cy.get('[data-testid=menu-login-button]').should('be.visible');
 });
 
 When('they look at any page in NVA', () => {
-  cy.get('@authResult').then((authResult) => {
-    const uuid = uuidv4();
-    const auth = `Bearer ${authResult.AccessToken}`;
-    cy.visit({
-      url: `/${uuid}`,
-      headers: {
-        Authorization: auth,
-      },
-    });
-  });
+  const uuid = uuidv4();
+  cy.visit(`/${uuid}`);
 });
+
 Then('they see the Log in button', () => {
   cy.get('[data-testid=menu-login-button]').should('be.visible');
 });
