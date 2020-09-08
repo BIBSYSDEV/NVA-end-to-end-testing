6855816e79c54459d45f3bc7787b6affeb6e6caa
Eiriknil <eirik.nilsen@unit.no>
2020-09-08 12:00:04 +0200

    Extracted env variables to constants

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,15 +2,18 @@ import AWS from 'aws-sdk';
 import { v4 as uuidv4 } from 'uuid';
 import Amplify, { Auth } from 'aws-amplify';
 
+const AWS_ACCESS_KEY_ID = Cypress.env('AWS_ACCESS_KEY_ID');
+const AWS_SECRET_ACCESS_KEY = Cypress.env('AWS_SECRET_ACCESS_KEY');
+const AWS_SESSION_TOKEN = Cypress.env('AWS_SESSION_TOKEN');
 const REGION = Cypress.env('AWS_REGION');
 const IDENTITY_POOL_ID = Cypress.env('AWS_IDENTITY_POOL_ID');
 const USER_POOL_ID = Cypress.env('AWS_USER_POOL_ID');
 const CLIENT_ID = Cypress.env('AWS_CLIENT_ID');
 
 AWS.config = new AWS.Config({
-  accessKeyId: Cypress.env('AWS_ACCESS_KEY_ID'),
-  secretAccessKey: Cypress.env('AWS_SECRET_ACCESS_KEY'),
-  sessionToken: Cypress.env('AWS_SESSION_TOKEN'),
+  accessKeyId: AWS_ACCESS_KEY_ID,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  sessionToken: AWS_SESSION_TOKEN,
   region: REGION,
 });
 
