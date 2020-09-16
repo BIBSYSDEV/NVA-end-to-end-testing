e4f0e8e556760dc6a72f3cd2bc0d7e58b8552a5c
Eiriknil <eirik.nilsen@unit.no>
2020-09-16 12:54:33 +0200

    NVA-features as submodule. Mechanism to fetch specific features.

---
+++ scripts/remove-old-features.js
@@ -0,0 +1,41 @@
+const glob = require('glob');
+const fs = require('fs');
+const testedFeatures = require('./testedFeatures.json');
+
+const CYPRESS_DIR = 'cypress/integration';
+const GHERKIN_DIR = 'features';
+
+const getDirectories = (src, callback) => {
+  glob(`${src}/**/*.feature`, callback);
+};
+
+const isInTestedFeatures = (filePath) => {
+  let isInTested = false;
+  testedFeatures.forEach((feature) => {
+    console.log(feature);
+    if (filePath.includes(feature)) {
+      console.log(filePath);
+      return (isInTested = true);
+    }
+  });
+  return isInTested;
+};
+
+const isFileToDelete = (f) => {
+  const filename = f.replace(CYPRESS_DIR, '');
+  const gherkinFile = `${GHERKIN_DIR}${filename}`;
+  console.log(isInTestedFeatures(gherkinFile));
+  const exists = fs.existsSync(gherkinFile) && isInTestedFeatures(gherkinFile);
+  console.log(`${f} exists: ${exists}`);
+  return !exists;
+};
+
+const deleteFile = (f) => {
+  console.log('Deleting old feature file', f);
+  fs.unlinkSync(f);
+};
+
+getDirectories(CYPRESS_DIR, (err, res) => {
+  const filesToDelete = res.filter(isFileToDelete);
+  filesToDelete.forEach(deleteFile);
+});
