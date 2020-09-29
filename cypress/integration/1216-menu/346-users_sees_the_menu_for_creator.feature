1b7fe065732b15587f0620a12589cba49563bbfc
Eiriknil <eirik.nilsen@unit.no>
2020-09-29 08:36:19 +0200

    Test for creator menu

---
+++ cypress/integration/1216-menu/346-users_sees_the_menu_for_creator.feature
@@ -0,0 +1,13 @@
+Feature: User sees the menu for Creator
+
+    @346
+    Scenario: User sees the menu for Creator
+        Given that the user is logged in
+        And they have the role of Creator
+        When they look at any page in NVA
+        Then they see a menu containing
+            | My profile      |
+            | New publication |
+            | My publications |
+            # | My Messages      |
+            | Log out         |
---
+++ cypress/integration/1216-menu/346-users_sees_the_menu_for_creator/menu_for_creator.js
@@ -0,0 +1,29 @@
+import { Given, And, When, Then } from 'cypress-cucumber-preprocessor/steps';
+import { USER_WITH_AUTHOR } from '../../../support/constants';
+import { v4 as uuidv4 } from 'uuid';
+import 'cypress-localstorage-commands';
+
+Given('that the user is logged in', () => {
+  cy.loginCognito(USER_WITH_AUTHOR).then((idToken) => {
+    cy.wrap(idToken).as('idToken');
+    cy.setLocalStorage('i18nextLng', 'eng');
+    cy.setLocalStorage('previouslyLoggedIn', 'true');
+    cy.visit('/');
+  });
+});
+And('they have the role of Creator', () => {});
+When('they look at any page in NVA', () => {
+  cy.visit(`/${uuidv4()}`);
+});
+Then('they see a menu containing', (dataTable) => {
+  dataTable.rawTable.forEach((value) => {
+    cy.get('[data-testid=menu]').click({ force: true });
+    cy.get('ul[role=menu]').within(() => {
+      cy.get('li[role=menuitem]').contains(value[0]);
+    });
+  });
+});
+// | My Profile       |
+// | New Registration |
+// | My Publications  |
+// | Log Out          |
