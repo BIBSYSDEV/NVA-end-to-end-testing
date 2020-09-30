79f3dec530c8e267d85c078afee7c1ecc461a8c3
Eiriknil <eirik.nilsen@unit.no>
2020-09-30 09:56:44 +0200

    Tests for curator menu

---
+++ cypress/integration/1216-menu/347-user_sees_the_menu_for_curator/menu_for_curator.js
@@ -0,0 +1,28 @@
+import { Given, And, When, Then } from 'cypress-cucumber-preprocessor/steps';
+import { USER_CURATOR_WITH_AUTHOR, USER_WITH_AUTHOR } from '../../../support/constants';
+import { v4 as uuidv4 } from 'uuid';
+import 'cypress-localstorage-commands';
+
+Given('that the user is logged in', () => {
+  cy.loginCognito(USER_CURATOR_WITH_AUTHOR).then((idToken) => {
+    cy.wrap(idToken).as('idToken');
+    cy.setLocalStorage('i18nextLng', 'eng');
+    cy.setLocalStorage('previouslyLoggedIn', 'true');
+    cy.visit('/');
+  });
+});
+And('they have the role of Curator', () => {});
+When('they look at any page in NVA', () => {
+  cy.visit(`/${uuidv4()}`);
+});
+Then('they see a menu containing', (dataTable) => {
+  dataTable.rawTable.forEach((value) => {
+    cy.get('[data-testid=menu]').click({ force: true });
+    cy.get('ul[role=menu]').within(() => {
+      cy.get('li[role=menuitem]').contains(value[0]);
+    });
+  });
+});
+// | My profile  |
+// | My worklist |
+// | Log out     |
