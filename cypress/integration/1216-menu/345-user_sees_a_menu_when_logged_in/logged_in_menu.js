5eec1341930d2bd660418f2091fbe752d3ac5dd4
Eiriknil <eirik.nilsen@unit.no>
2020-10-02 11:01:12 +0200

    Tests for menu

---
+++ cypress/integration/1216-menu/345-user_sees_a_menu_when_logged_in/logged_in_menu.js
@@ -0,0 +1,25 @@
+import { And, Given, Then, When } from 'cypress-cucumber-preprocessor/steps';
+import { USER_WITH_AUTHOR } from '../../../support/constants';
+import { v4 as uuidV4 } from 'uuid';
+import 'cypress-localstorage-commands';
+
+Given('that the user is logged in', () => {
+  cy.loginCognito(USER_WITH_AUTHOR).then((idToken) => {
+    cy.wrap(idToken).as('idToken');
+    cy.setLocalStorage('i18nextLng', 'eng');
+    cy.setLocalStorage('previouslyLoggedIn', 'true');
+    cy.visit('/');
+  });
+});
+And('they have no NVA role', () => {});
+When('they look at any page in NVA', () => {
+  cy.visit(`/${uuidV4()}`);
+});
+Then('they see a menu containing', (dataTable) => {
+  cy.get('[test-dataid=menu]').click({ force: true });
+  dataTable.rawTable.forEach((value) => {
+    cy.get(`[data-testid]`).contains(value[0]);
+  });
+});
+// | My profile |
+// | Log out    |
