6d4eb4d6307167d6e0f07cb008c16102edc765c9
Eiriknil <eirik.nilsen@unit.no>
2020-10-05 08:47:58 +0200

    tests for app-admin menu

---
+++ cypress/integration/1216-menu/350-user_sees_the_menu_for_application_administrator/menu_for_app_admin.js
@@ -0,0 +1,28 @@
+import { Given, And, When, Then } from 'cypress-cucumber-preprocessor/steps';
+import { ADMIN_USER } from '../../../support/constants';
+import { v4 as uuidv4 } from 'uuid';
+import 'cypress-localstorage-commands';
+
+Given('that the user is logged in', () => {
+  cy.loginCognito(ADMIN_USER).then((idToken) => {
+    cy.wrap(idToken).as('idToken');
+    cy.setLocalStorage('i18nextLng', 'eng');
+    cy.setLocalStorage('previouslyLoggedIn', 'true');
+    cy.visit('/');
+  });
+});
+And('they have the role of Application administrator', () => {});
+When('they look at any page in NVA', () => {
+  cy.visit(`/${uuidv4()}`);
+});
+Then('they see a menu containing', (dataTable) => {
+  dataTable.rawTable.forEach((value) => {
+    cy.get('[data-testid=menu]').click({ force: true });
+    cy.get('ul[role=menu]').within(() => {
+      cy.get('li[role=menuitem]').contains(value[0]);
+    });
+  });
+});
+// | My Profile   |
+// | Institutions |
+// | Log Out      |
