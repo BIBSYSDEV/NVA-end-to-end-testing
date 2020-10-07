4854396e67b2e7a69ab8f7467f06c607e0768e70
Eiriknil <eirik.nilsen@unit.no>
2020-10-07 13:07:41 +0200

    Test for Add institution

---
+++ cypress/integration/1217-my_profile_page/410-user_opens_add_institution_from_my_profile/open_add_institution.js
@@ -0,0 +1,21 @@
+import { And, Given, Then, When } from 'cypress-cucumber-preprocessor/steps';
+import { USER_WITH_AUTHOR } from '../../../support/constants';
+import 'cypress-localstorage-commands';
+
+Given('user opens the page My Profile', () => {
+  cy.loginCognito(USER_WITH_AUTHOR).then((idToken) => {
+    cy.wrap(idToken).as('idToken');
+    cy.setLocalStorage('i18nextLng', 'eng');
+    cy.setLocalStorage('previouslyLoggedIn', 'true');
+    cy.visit('/');
+    cy.get('[data-testid=menu]').click({ force: true });
+    cy.get('[data-testid=menu-user-profile-button]').click({ froce: true });
+  });
+});
+
+When('they click Add Institution', () => {
+  cy.get('[data-testid=add-new-institution-button]').should('not.be.disabled').click({ force: true });
+});
+Then('they see the Autosearch box for Institutions', () => {
+  cy.get('[data-testid=autocomplete-institution]').should('be.visible');
+});
