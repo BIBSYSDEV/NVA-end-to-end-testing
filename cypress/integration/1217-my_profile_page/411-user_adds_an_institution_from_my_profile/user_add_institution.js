67af52e7ce1b75443c02612af759cf828f05e887
Eiriknil <eirik.nilsen@unit.no>
2020-10-07 14:43:32 +0200

    Test for add institution

--- cypress/integration/1217-my_profile_page/411-user_adds_an_institution_from_my_profile/user_add_institution.js
+++ cypress/integration/1217-my_profile_page/411-user_adds_an_institution_from_my_profile/user_add_institution.js
@@ -1,24 +1,41 @@
 import { And, Given, Then, When } from 'cypress-cucumber-preprocessor/steps';
-import { USER_WITH_AUTHOR } from '../../../support/constants';
+import { USER_ADD_INSTITUTION } from '../../../support/constants';
 import 'cypress-localstorage-commands';
 
 Given('User opens Add Institution from My Profile', () => {
-  cy.loginCognito(USER_WITH_AUTHOR).then((idToken) => {
+  cy.loginCognito(USER_ADD_INSTITUTION).then((idToken) => {
     cy.wrap(idToken).as('idToken');
     cy.setLocalStorage('i18nextLng', 'eng');
     cy.setLocalStorage('previouslyLoggedIn', 'true');
     cy.visit('/');
     cy.get('[data-testid=menu]').click({ force: true });
     cy.get('[data-testid=menu-user-profile-button]').click({ force: true });
-    cy.get('[data-testid=add-new-institution-button]').should('not.be.disabled').click({ force: true });
+    cy.get('[data-testid=add-new-institution-button]').should('not.be.disabled');
+    cy.get('[data-testid=add-new-institution-button]').click();
   });
 });
 
 When('they enter an Institution name', () => {
-  cy.get('[data-testid=autocomplete-institution]').click({ force: true }).type('testing');
+  cy.get('[data-testid=autocomplete-institution]').type('ntnu');
 });
-And('they select an Institution', () => {});
-And('they click Save', () => {});
-Then('they see the Add Institution dialog is closed', () => {});
-And('they see the new Institution in My Profile', () => {});
-And('they see a button Remove that is enabled for the new Institution', () => {});
+And('they select an Institution', () => {
+  cy.contains('Norges teknisk-naturvitenskapelige universitet').click({ force: true });
+});
+And('they see a button Add that is enabled for the new Institution', () => {
+  cy.get('[data-testid=institution-add-button]').should('be.visible');
+});
+And('they see a button Cancel that is enabled for the new Institution', () => {
+  cy.get('[data-testid=institution-cancel-button]').should('be.visible');
+});
+And('the click on the Add button', () => {
+  cy.get('[placeholder="Search for department"]').should('be.visible');
+  cy.get('[data-testid=institution-add-button]').click({ force: true });
+});
+Then('they see the new Institution in My Profile', () => {
+  cy.contains('Norges teknisk-naturvitenskapelige universitet');
+});
+And('they see the Remove button', () => {
+  cy.get('[data-testid^=button-delete-institution]').should(($delete) => {
+    expect($delete).to.have.length(2);
+  });
+});
