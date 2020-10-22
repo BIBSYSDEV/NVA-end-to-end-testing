521dfef5e2a2117e8b80e8d1969481828414ecb7
Eirik Nilsen <eirik.nilsen@unit.no>
2020-10-22 10:24:20 +0200

    Np 1219 test user sees a subsubsubunit from my profile (#32)
    * See and select subunit test
    
    * Test to see subsubunit
    
    * Better selection of autocomplete
    
    * Test to add subsubunit
    
    * Test for see subsubsubunit
    
    * Test for select subsubsubunit

---
+++ cypress/integration/1217-my_profile_page/1219-user_sees_a_subsubsubunit_from_my_profile/see_subsubsubunit.js
@@ -0,0 +1,48 @@
+import { After, And, Given, Then, When } from 'cypress-cucumber-preprocessor/steps';
+import { USER_ADD_INSTITUTION } from '../../../support/constants';
+import 'cypress-localstorage-commands';
+
+Given('they see a Subsubunit from My Profile', () => {
+  cy.loginCognito(USER_ADD_INSTITUTION).then((idToken) => {
+    cy.wrap(idToken).as('idToken');
+    cy.setLocalStorage('i18nextLng', 'eng');
+    cy.setLocalStorage('previouslyLoggedIn', 'true');
+    cy.visit('/');
+    cy.get('[data-testid=menu]').click({ force: true });
+    cy.get('[data-testid=menu-user-profile-button]').click({ force: true });
+    cy.get('[data-testid=add-new-institution-button]').should('not.be.disabled');
+    cy.get('[data-testid=add-new-institution-button]').click();
+    cy.get('[data-testid=autocomplete-institution]').type('ntnu');
+    cy.contains('Norges teknisk-naturvitenskapelige universitet').click({ force: true });
+    cy.contains('Department').should('be.visible');
+    cy.get('[data-testid=autocomplete-institution]')
+      .last()
+      .type('Fakultet for informasjonsteknologi og elektroteknikk');
+    cy.contains('Fakultet for informasjonsteknologi og elektroteknikk').click({ force: true });
+  });
+});
+
+And('they select a Subsubunit from the Subsubunit dropdown', () => {
+  cy.get('[data-testid=autocomplete-institution]').last().type('IE fakultetsadministrasjon');
+  cy.contains('IE fakultetsadministrasjon').click({ force: true });
+});
+And('the see the Subsubsubunit dropdown', () => {
+  cy.get('[data-testid=autocomplete-institution]').last().should('have.value', '');
+});
+When('they click the Subsubsubunit dropdown', () => {
+  cy.get('[data-testid=autocomplete-institution]')
+    .last()
+    .within(($autocomplete) => {
+      cy.wrap($autocomplete).should('have.value', '');
+      cy.wrap($autocomplete).type('realfagsrekruttering');
+    });
+});
+
+Then('they see a Subsubsubunit dropdown containing all the Subsubsubunits at their Subsubunit', () => {
+  cy.contains('Nasjonalt senter for realfagsrekruttering');
+});
+
+After(() => {
+  // cancel select institution to reset test user
+  cy.get('[data-testid=institution-cancel-button]').click({ force: true });
+});
