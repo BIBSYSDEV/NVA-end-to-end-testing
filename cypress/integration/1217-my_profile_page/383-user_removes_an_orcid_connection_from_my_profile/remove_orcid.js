4b0a94d3fd37a7eafeecc41daeb886bcd6615fa4
Eirik Nilsen <eirik.nilsen@unit.no>
2020-12-02 14:11:36 +0100

    Test remove orcid

--- cypress/integration/1217-my_profile_page/383-user_removes_an_orcid_connection_from_my_profile/remove_orcid.js
+++ cypress/integration/1217-my_profile_page/383-user_removes_an_orcid_connection_from_my_profile/remove_orcid.js
@@ -2,9 +2,15 @@ import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
 import { USER_REMOVE_ORCID } from '../../../support/constants';
 
 Given('user opens the page My Profile', () => {
-  cy.login(USER_REMOVE_ORCID);
-  cy.get('[data-testid=menu]').click({ force: true });
-  cy.get('[data-testid=menu-user-profile-button]').click({ force: true });
+  cy.login(USER_REMOVE_ORCID).then(() => {
+    cy.get('[data-testid=menu]').click({ force: true });
+    cy.get('[data-testid=menu-user-profile-button]').click({ force: true });
+  });
 });
-When('they click Remove ORCID', () => {});
-Then('they see a confirmation that the ORCID is removed', () => {});
+When('they click Remove ORCID', () => {
+  cy.get('[data-testid=button-confirm-delete-orcid]').click({ force: true });
+  cy.get('[data-testid=accept-button]').click({ force: true });
+});
+Then('they see a confirmation that the ORCID is removed', () => {
+  cy.contains('Deleted identifier');
+});
