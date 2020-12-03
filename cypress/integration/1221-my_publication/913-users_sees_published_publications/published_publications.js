1938a8c3aca1fed22721260c06d946b4581a9c30
Eirik Nilsen <eirik.nilsen@unit.no>
2020-12-03 08:12:58 +0100

    Test for published registrations

--- cypress/integration/1221-my_publication/913-users_sees_published_publications/published_publications.js
+++ cypress/integration/1221-my_publication/913-users_sees_published_publications/published_publications.js
@@ -1,13 +1,22 @@
 import { And, Given, Then, When } from 'cypress-cucumber-preprocessor/steps';
 import { USER_WITH_AUTHOR } from '../../../support/constants';
-import 'cypress-localstorage-commands';
 
-Given('that the user is logged in as Creator', () => {
-  cy.loginCognito(USER_WITH_AUTHOR).then((idToken) => {
-    cy.wrap(idToken).as('idToken');
-    cy.setLocalStorage('i18nextLng', 'eng');
-    cy.setLocalStorage('previouslyLoggedIn', 'true');
-    cy.setLocalStorage('amplify-signin-with-hostedUI', 'true');
-    cy.visit('/');
-  });
+Given('Creator opens the page My Publications', () => {
+  cy.login(USER_WITH_AUTHOR);
+  cy.get('[data-testid=my-registrations]').click({ force: true });
 });
+When('they click Published registrations in the navigation bar', () => {});
+Then('they see a list of all published registrations with the fields', () => {});
+// | Title      |
+// | "<Status>" |
+// | Created    |
+And('they see each list item has buttons Delete and Edit', () => {});
+And('the they see the Edit button is enabled', () => {});
+And('the Delete button is enabled for registrations not marked as Deleted', () => {});
+And('they see the navigation bar for unpublished registrations is enabled', () => {});
+And('they see the navigation bar for published registrations is selected', () => {});
+
+// Examples:
+//     | Status    |
+//     | Deleted   |
+//     | Published |
