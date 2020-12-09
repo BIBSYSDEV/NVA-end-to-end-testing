79172a86447965e8dd1d26013389d54694c7d382
Eirik Nilsen <eirik.nilsen@unit.no>
2020-12-09 12:41:52 +0100

    Fixed test with revised scenario

--- cypress/integration/1224-user_administration/359-administrator_views_user_administration_sections_with_role_members_part_i/view_user_administration.js
+++ cypress/integration/1224-user_administration/359-administrator_views_user_administration_sections_with_role_members_part_i/view_user_administration.js
@@ -1,24 +1,15 @@
 import { Given, When, Then, And } from 'cypress-cucumber-preprocessor/steps';
 import { USER_INST_ADMIN_WITH_AUTHOR } from '../../../support/constants';
-
-const SECTION_HEADINGS = {
-  'Institution Administrators': 'Administrators',
-  'Curators': 'Curators',
-  'Editors': 'Editors',
-  'Creators': 'Registrator',
-};
-
-const ROLE_BUTTONS = {
-  'Administrator': 'button-add-institution-admin',
-  'Curator': 'button-add-curator',
-  'Editor': 'button-add-editor',
-  'Creator': '',
-};
+import {
+  USER_ADMINISTRAION_BUTTONS,
+  USER_ADMINISTRATION_HEADINGS,
+  USER_ADMINISTRATION_REMOVE_ROLE_BUTTONS,
+} from '../../../support/data_testid_constants';
 
 Given('that the user is logged in as Administrator', () => {
   cy.login(USER_INST_ADMIN_WITH_AUTHOR);
 });
-When('they click the menu item User Administration', () => {
+When('they click the menu item Users', () => {
   cy.get('[data-testid=menu]').click({ force: true });
   cy.get('[data-testid=menu-admin-institution-users-button]').click({ force: true });
 });
@@ -26,22 +17,34 @@ Then('they see the User Administration page', () => {
   cy.location('pathname').should('equal', '/my-institution-users');
 });
 And('they see the {string}', (section) => {
-  cy.get('h3').contains(SECTION_HEADINGS[section]);
+  cy.get('h3').contains(USER_ADMINISTRATION_HEADINGS[section]).parent().as('section');
+  cy.wrap(section).as('sectionName');
 });
 And(
   'they see the {string} contains a list of all users affiliated with their institution and with with role {string}',
-  (dataTable) => {}
+  (section, role) => {
+    cy.get('@section').get('tr').as('userList').should('have.length.above', 0);
+  }
 );
 And('they see under each Section a Button to assign the {string} to a another user', (role) => {
-  if (role !== 'Creator') {
-    cy.get(`[data-testid=${ROLE_BUTTONS[role]}]`).should('be.visible');
-  }
+  cy.get(`[data-testid=${USER_ADMINISTRAION_BUTTONS[role]}]`).should('be.visible');
 });
-And('they see that each list has the field "Authentication ID"', () => {});
-And('they see a button Remove that is enabled for each user', () => {});
+And('they see that each list has the field "Username"', () => {
+  cy.get('@userList').parent().get('thead').contains('Username');
+});
+And('they see a button Remove that is enabled for each user', () => {
+  cy.get('@section').within((section) => {
+    cy.get('@sectionName').then((sectionName) => {
+      cy.get(`[data-testid^=${USER_ADMINISTRATION_REMOVE_ROLE_BUTTONS[sectionName]}]`).should('be.enabled');
+    });
+  });
+});
+And('they see a section Registrator with a policy for who are able to publish', () => {
+  cy.get('h3').contains('Registrator');
+  cy.get('[data-testid=checkbox-assign-creators]');
+});
 // Examples:
 //     | Section                    | Role          |
 //     | Institution Administrators | Administrator |
 //     | Curators                   | Curator       |
 //     | Editors                    | Editor        |
-//     | Creators                   | Creator       |
