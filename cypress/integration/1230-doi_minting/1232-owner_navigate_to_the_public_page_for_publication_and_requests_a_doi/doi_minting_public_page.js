aa33b0e5d2df95a837287e7e5cf4990884f504fe
Eiriknil <eirik.nilsen@unit.no>
2021-01-25 08:20:45 +0100

    Doi minting

--- cypress/integration/1230-doi_minting/1232-owner_navigate_to_the_public_page_for_publication_and_requests_a_doi/doi_minting_public_page.js
+++ cypress/integration/1230-doi_minting/1232-owner_navigate_to_the_public_page_for_publication_and_requests_a_doi/doi_minting_public_page.js
@@ -5,10 +5,23 @@ Given('that the Creator navigates to the Public Page for Publication for publish
   cy.login(USER_WITH_AUTHOR);
 });
 And('they are the Owner of the Publication', () => {});
-And('open "Request a DOI" dialog', () => {});
-And('optional add a message to the Curator', () => {});
-When('the user click the Send Button', () => {});
-Then('the Public Page for Publication is displayed', () => {});
-And('the "Request a DOI" button is renamed to "DOI pending" and is disabled', () => {});
-And('the request is listed in User Worklist', () => {});
+And('open "Request a DOI" dialog', () => {
+  cy.get('[data-testid=button-toggle-request-doi]').click({ force: true });
+});
+And('optional add a message to the Curator', () => {
+  cy.get('textarea').type('Optional message');
+});
+When('the user click the Send Button', () => {
+  cy.get('[data-testid=button-send-doi-request]'); // TODO find out how to test without creating a multitude of doi
+});
+Then('the Public Page for Publication is displayed', () => {
+  cy.location('pathname').should('contain', 'public');
+});
+And('the "Request a DOI" button is renamed to "DOI pending" and is disabled', () => {
+  cy.get('[data-testid=button-toggle-request-doi]').should('have.text', 'DOI pending');
+  cy.get('[data-testid=button-toggle-request-doi]').should('be.disabled');
+});
+And('the request is listed in User Worklist', () => {
+    
+});
 And('the request is listed in Curator Worklist', () => {});
