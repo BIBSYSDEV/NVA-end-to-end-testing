369a48019a8b9618b758ef7f70500657f63b873c
Eiriknil <eirik.nilsen@unit.no>
2021-01-28 13:46:52 +0100

    Test for request DOI dialog

---
+++ cypress/integration/1230-doi_minting/511-owner_opens_the_request_a_doi_dialog/request_a_doi.js
@@ -0,0 +1,27 @@
+import { Given, And, When, Then } from 'cypress-cucumber-preprocessor/steps';
+import { USER_WITH_AUTHOR } from '../../../support/constants';
+
+Given('that a Creator navigates to the Public Page for Publication for published publication without DOI', () => {
+  cy.login(USER_WITH_AUTHOR);
+  cy.get('[data-testid=my-registrations]').click({ force: true });
+  cy.get('[data-testid=published-button]').click({ force: true });
+  cy.get('[data-testid^=registration-title]')
+    .filter(':contains("Published publication without DOI")')
+    .parent()
+    .within((presentationLine) => {
+      cy.wrap(presentationLine).get('[data-testid^=open-registration]').click({ force: true });
+    });
+});
+And('they are the Owner of this Publication', () => {});
+And('they click the "Request a DOI" button', () => {
+  cy.get('[data-testid=button-toggle-request-doi]').click({ force: true });
+});
+Then('the "Request a DOI dialog" is opened', () => {
+  cy.get('[data-testid=request-doi-modal]').should('be.visible');
+});
+And('they see fields for Message', () => {
+    cy.get('[data-testid=request-doi-message]').should('be.visible');
+});
+And('they see a "Send Request" button', () => {
+  cy.get('[data-testid=button-send-doi-request]').should('be.visible');
+});
