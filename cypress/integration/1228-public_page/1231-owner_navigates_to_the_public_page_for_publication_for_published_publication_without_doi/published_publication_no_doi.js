245bd954fd65c613b3bbe6e9bc3229b9b3dec15c
Eiriknil <eirik.nilsen@unit.no>
2021-01-28 12:52:29 +0100

    Cleaner code

--- cypress/integration/1228-public_page/1231-owner_navigates_to_the_public_page_for_publication_for_published_publication_without_doi/published_publication_no_doi.js
+++ cypress/integration/1228-public_page/1231-owner_navigates_to_the_public_page_for_publication_for_published_publication_without_doi/published_publication_no_doi.js
@@ -6,8 +6,7 @@ Given('that the Creator navigates to the Public Page for Publication', () => {
   cy.get('[data-testid=my-registrations]').click({ force: true });
   cy.get('[data-testid=published-button]').click({ force: true });
   cy.get('[data-testid^=registration-title]')
-    .contains('Published publication without DOI')
-    .parent()
+    .filter(':contains("Published publication without DOI")') // need to use text search to find correct registration
     .parent()
     .within((publicationLine) => {
       cy.wrap(publicationLine).get('[data-testid^=open-registration]').click({ force: true });
@@ -15,10 +14,10 @@ Given('that the Creator navigates to the Public Page for Publication', () => {
 });
 And('they are the Owner of this Publication', () => {});
 And('the Publication has no DOI', () => {
-  cy.contains('DOI to resource:').should('not.exist'); // add data-testid to frontend
+  cy.get('[data-testid=doi-presentation]').should('not.exist');
 });
 When('they see the Status Bar', () => {
-  cy.contains('Status:'); // add data-testid to frontend
+  cy.get('[data-testid=public-registration-status]');
 });
 Then('they see buttons for "Request a DOI" and "Edit Publication"', () => {
   cy.get('[data-testid=button-toggle-request-doi]').should('be.visible');
