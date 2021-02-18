f5824e7f9fb6879b1a9062c8675dd6b6b5d0f3a3
Eirik Nilsen <eirik.nilsen@unit.no>
2021-02-18 13:48:59 +0100

    Tests for login, waiting for update in frontend

--- cypress/integration/24-authentication/user-logs-in.feature
+++ cypress/integration/24-authentication/user-logs-in.feature
@@ -8,33 +8,33 @@ Feature: User logs in
     And they see a Create New Author Button
     And they see a Support button
 
-  # @1206
-  # Scenario: User with their Feide ID in ARP logs in
-  #   Given that the user logs in with their Feide ID
-  #   And their Feide ID is in an ARP entry
-  #   Then they can see their name in the menu
+  @1206
+  Scenario: User with their Feide ID in ARP logs in
+    Given that the user logs in with their Feide ID
+    And their Feide ID is in an ARP entry
+    Then they can see their name in the menu
 
-  # @384
-  # Scenario: User creates a new Author identity
-  #   Given that the user logs in with their Feide ID
-  #   And they do not have their Feide ID in any ARP entry
-  #   Then they see proposed name for a new Author identity based on data from their Feide account
-  #   When they click Create Author identity button
-  #   Then this new Author identity is added to ARP
-  #   And their Feide ID is added to their Author identity
-  #   And their Organization ID (Cristin ID) is added to their Author identity
-  #   And they can see confirmation message that they have connected an Author identity
+  @384
+  Scenario: User creates a new Author identity
+    Given that the user logs in with their Feide ID
+    And they do not have their Feide ID in any ARP entry
+    Then they see proposed name for a new Author identity based on data from their Feide account
+    When they click Create Author identity button
+    Then this new Author identity is added to ARP
+    And their Feide ID is added to their Author identity
+    And their Organization ID (Cristin ID) is added to their Author identity
+    And they can see confirmation message that they have connected an Author identity
 
-  # @219
-  # Scenario: User updates an Author identity
-  #   Given that the user logs in with their Feide ID
-  #   And they do not have their Feide ID in any ARP entry
-  #   And they see a list containing "Name", "Last registration" and "Institutions" for each ARP entry matching their name
-  #   When they select an Author identity
-  #   And they click Connect Author identity
-  #   Then their Feide ID is added to their Author identity
-  #   And their Organization ID (Cristin ID) is added to their Author identity
-  #   And they can see confirmation message that they have connected an Author identity
+  @219
+  Scenario: User updates an Author identity
+    Given that the user logs in with their Feide ID
+    And they do not have their Feide ID in any ARP entry
+    And they see a list containing "Name", "Last registration" and "Institutions" for each ARP entry matching their name
+    When they select an Author identity
+    And they click Connect Author identity
+    Then their Feide ID is added to their Author identity
+    And their Organization ID (Cristin ID) is added to their Author identity
+    And they can see confirmation message that they have connected an Author identity
 
   @222
   Scenario: User adds an ORCID to their Author identity
--- cypress/integration/24-authentication/user-logs-in/user_logs_in.js
+++ cypress/integration/24-authentication/user-logs-in/user_logs_in.js
@@ -16,12 +16,20 @@ And('they do not have their Feide ID in any ARP entry', () => {
 });
 Then(
   'they see a list containing "Name", "Last registration" and "Institutions" for each ARP entry matching their name',
-  () => {}
+  () => {
+    cy.get('[data-testid=connect-author-modal]').within((orcidModal) => {
+      cy.get(orcidModal).contains('Name');
+      cy.get(orcidModal).contains('Last registration');
+      cy.get(orcidModal).contains('Organizations');
+    });
+  }
 );
 And('they see a Create New Author Button', () => {
   cy.get('[data-testid=button-create-authority]').should('be.visible');
 });
-And('they see a Support button', () => {});
+And('they see a Support button', () => {
+  // TODO Support button not implemented
+});
 
 // @1206
 // Scenario: User with their Feide ID in ARP logs in
@@ -32,36 +40,55 @@ Then('they can see their name in the menu', () => {
   cy.get('[data-testid=menu]').contains('Withauthor TestUser');
 });
 
+// Common steps for @384 and @219
+And('their Feide ID is added to their Author identity', () => {
+  cy.get('@authority').its('feideids').should('have.length', 1);
+});
+And('their Organization ID \\(Cristin ID) is added to their Author identity', () => {
+  cy.get('@authority').its('orgunitids').should('have.length', 1);
+});
+
 // @384
 // Scenario: User creates a new Author identity
-And('they do not have their Feide ID in any ARP entry', () => {
-  cy.login(USER_NO_NAME_IN_ARP);
-});
+And('they do not have their Feide ID in any ARP entry', () => {});
 Then('they see proposed name for a new Author identity based on data from their Feide account', () => {
+  cy.login(USER_NO_NAME_IN_ARP);
   cy.get('[data-testid=connect-author-modal]').contains('TestUser, No name in ARP');
 });
 When('they click Create Author identity button', () => {
   cy.get('[data-testid=button-create-authority]').click({ force: true });
 });
 Then('this new Author identity is added to ARP', () => {});
-And('their Feide ID is added to their Author identity', () => {});
-And('their Organization ID \\(Cristin ID) is added to their Author identity', () => {});
-And('they can see confirmation message that they have connected an Author identity', () => {});
+And('they can see confirmation message that they have connected an Author identity', () => {
+  cy.get('[data-testid=connect-author-modal]').contains('Your author identity is connected');
+});
 
 // @219
 // Scenario: User updates an Author identity
-And('they do not have their Feide ID in any ARP entry', () => {
-  cy.login(USER_NO_ARP);
-});
+And('they do not have their Feide ID in any ARP entry', () => {});
 And(
   'they see a list containing "Name", "Last registration" and "Institutions" for each ARP entry matching their name',
-  () => {}
+  () => {
+    cy.login(USER_NO_ARP);
+    cy.get('[data-testid=connect-author-modal]').within((connectAuthor) => {
+      cy.wrap(connectAuthor).contains('Name');
+      cy.wrap(connectAuthor).contains('Last registration');
+      cy.wrap(connectAuthor).contains('Organizations');
+    });
+  }
 );
-When('they select an Author identity', () => {});
-And('they click Connect Author identity', () => {});
-Then('their Feide ID is added to their Author identity', () => {});
-And('their Organization ID (Cristin ID) is added to their Author identity', () => {});
-And('they can see confirmation message that they have connected an Author identity', () => {});
+When('they select an Author identity', () => {
+  cy.get('[data-testid=connect-author-modal]').get('[data-testid^=author-radio-button]').first().click({ force: true });
+});
+And('they click Connect Author identity', () => {
+  cy.get('[data-testid=connect-author-button]').should('be.enabled');
+  cy.get('[data-testid=connect-author-button]').click({ force: true });
+  cy.get('[data-testid=modal_next]').should('be.visible'); // Wait until next modal is visible to avoid race condition
+  cy.window().its('store').invoke('getState').its('user').its('authority').as('authority');
+});
+And('they can see confirmation message that they have connected an Author identity', () => {
+  cy.get('[data-testid=connect-author-modal]').contains('Your author identity is connected');
+});
 
 // @222
 // Scenario: User adds an ORCID to their Author identity
