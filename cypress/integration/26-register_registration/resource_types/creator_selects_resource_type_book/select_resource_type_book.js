0c973290d8ee0eb040ee335076b7c779c59a7105
Eiriknil <eirik.nilsen@unit.no>
2022-01-10 09:44:20 +0100

    Last data-testids from const

--- cypress/integration/26-register_registration/resource_types/creator_selects_resource_type_book/select_resource_type_book.js
+++ cypress/integration/26-register_registration/resource_types/creator_selects_resource_type_book/select_resource_type_book.js
@@ -7,13 +7,13 @@ import { dataTestId } from '../../../../support/dataTestIds';
 Before(() => {
   cy.login(userResourceType);
   cy.get(`[data-testid=${dataTestId.header.menuButton}]`).click();
-  cy.get('[data-testid=my-registrations-link]').click({ force: true });
+  cy.get(`[data-testid=${dataTestId.header.myRegistrationsLink}]`).click({ force: true });
   cy.get('[data-testid^=edit-registration]').first().click({ force: true });
 });
 
 // Common steps
 Given('Creator navigates to the Resource Type tab and selects Resource type "Book"', () => {
-  cy.get('[data-testid=nav-tabpanel-resource-type]').click({ force: true });
+  cy.get(`[data-testid=${dataTestId.registrationWizard.stepper.resourceStepButton}]`).click({ force: true });
   cy.get('[data-testid=publication-context-type]').click({ force: true }).type(' ');
   cy.get('[data-testid=publication-context-type-Book]').click({ force: true });
 });
@@ -26,7 +26,7 @@ When('they select Resource subtype {string}', (subtype) => {
 // TODO Missing subtypes Abstract collection, Exhibition catalog
 // Scenario: Creator navigates to the Resource Type tab and selects Resource type "Book"
 Given('Creator navigates to Resource Type tab', () => {
-  cy.get('[data-testid=nav-tabpanel-resource-type]').click({ force: true });
+  cy.get(`[data-testid=${dataTestId.registrationWizard.stepper.resourceStepButton}]`).click({ force: true });
 });
 When('they select the Resource type "Book"', () => {
   cy.get('[data-testid=publication-context-type]').click({ force: true }).type(' ');
