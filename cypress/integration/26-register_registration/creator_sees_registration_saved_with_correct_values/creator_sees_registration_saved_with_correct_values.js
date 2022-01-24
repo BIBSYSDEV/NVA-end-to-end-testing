e73efacb9c560cfa085fec0f17202d99f1c036c0
Eirik Nilsen <eirik.nilsen@unit.no>
2022-01-24 08:53:19 +0100

    Save registration book (#138)
    * Fill in fields for Book
    
    * Fill in all fields for Book
    
    * Fill in all fields for Book

--- cypress/integration/26-register_registration/creator_sees_registration_saved_with_correct_values/creator_sees_registration_saved_with_correct_values.js
+++ cypress/integration/26-register_registration/creator_sees_registration_saved_with_correct_values/creator_sees_registration_saved_with_correct_values.js
@@ -1,4 +1,5 @@
 import { userWithAuthor } from '../../../support/constants';
+import { dataTestId } from '../../../support/dataTestIds';
 
 // Feature: Creator sees registration is saved with correct values presented on landing page
 
@@ -7,15 +8,25 @@ Given('Author begins registering a registration', () => {
   cy.login(userWithAuthor);
   cy.startWizardWithEmptyRegistration();
 });
-And('selects {string} and {string}', (resourceType, subType) => {
-  cy.fillInCommonFields();
+And('selects {string} and {string}', (type, subType) => {
+  cy.wrap(type).as('type');
+  cy.wrap(subType).as('subtype');
 });
-And('fill in values for all fields', () => {});
+And('fill in values for all fields', () => {
+  cy.fillInCommonFields();
+  cy.get('@type').then((type) => {
+    cy.get('@subtype').then((subtype) => {
+      cy.fillInResourceType(type, subtype);
+    });
+  });
+});
 When('they saves registration', () => {
+  cy.get(`[data-testid=${dataTestId.registrationWizard.stepper.filesStepButton}]`).click();
   cy.get('[data-testid="button-save-registration"]').click();
 });
 Then('they can see the values on the Registration Landing Page', () => {
   cy.checkLandingPage();
 });
 // | Resource Type           | Subtype         |
-// | Contribution to journal | Journal article |
+// | Book                    | BookMonograph   |
+// | Book                    | BookAnthology   |
