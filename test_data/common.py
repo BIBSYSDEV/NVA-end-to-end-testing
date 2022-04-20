fd121eec1035d2ab2e74b7d15d4c6e258f9390e0
Eirik <eirik.nilsen@unit.no>
2022-04-20 12:36:17 +0200

    testing

--- test_data/common.py
+++ test_data/common.py
@@ -1,5 +1,6 @@
 import boto3
 import uuid
+import requests
 
 ssm = boto3.client('ssm')
 USER_POOL_ID = ssm.get_parameter(Name='/CognitoUserPoolId',
@@ -19,14 +20,24 @@ def login(username):
         Username=username,
         Permanent=True,
     )
-    response = client.admin_initiate_auth(
-        UserPoolId=USER_POOL_ID,
-        ClientId=CLIENT_ID,
-        AuthFlow='ADMIN_USER_PASSWORD_AUTH',
-        AuthParameters={
-            'USERNAME': username,
-            'PASSWORD': password
-        })
+    response = requests.post(
+        url='https://nva-dev.auth.eu-west-1.amazoncognito.com/login?client_id=406g9cf6bk5a17u96n93vb8k9m&response_type=code&scope=aws.cognito.signin.user.admin+email+https%3A%2F%2Fapi.nva.unit.no%2Fscopes%2Ffrontend+openid+phone&redirect_uri=https%3A%2F%2Flocalhost%3A3000',
+        json={
+            'username': username,
+            'password': password
+        }
+    )
+
+    print(response)
+
+    # response = client.admin_initiate_auth(
+    #     UserPoolId=USER_POOL_ID,
+    #     ClientId=CLIENT_ID,
+    #     AuthFlow='ADMIN_USER_PASSWORD_AUTH',
+    #     AuthParameters={
+    #         'USERNAME': username,
+    #         'PASSWORD': password
+    #     })
     return response['AuthenticationResult']['IdToken']
 
 def scan_customers():
