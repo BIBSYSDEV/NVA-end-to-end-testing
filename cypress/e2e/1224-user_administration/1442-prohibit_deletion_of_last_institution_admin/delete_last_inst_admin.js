18d911d08283a967d1c28746d58ff13472809679
Eirik <eirik.nilsen@unit.no>
2022-08-05 10:32:25 +0200

    Fix remove last admin

--- cypress/e2e/1224-user_administration/1442-prohibit_deletion_of_last_institution_admin/delete_last_inst_admin.js
+++ cypress/e2e/1224-user_administration/1442-prohibit_deletion_of_last_institution_admin/delete_last_inst_admin.js
@@ -1,11 +1,22 @@
-import { userInstAdminWithAuthor } from '../../../support/constants';
+import { adminUser } from '../../../support/constants';
 import { dataTestId } from '../../../support/dataTestIds';
 
 Given('that an App Admin or Institution Admin opens User Administration', () => {
-  cy.login(userInstAdminWithAuthor);
+  cy.login(adminUser);
   cy.get(`[data-testid=${dataTestId.header.basicDataLink}]`).click({ force: true });
 });
 And('they see only one current Institution Admin', () => {
+  cy.get(`[data-testid=${dataTestId.basicData.adminInstitutionsLink}]`).click();
+  cy.get('[data-testid=customer-institutions-list] > tbody > tr > td > p', { timeout: 30000 })
+    .filter(':contains("Test Institution")')
+    .first()
+    .parent()
+    .parent()
+    .within(() => {
+      cy.get(`[data-testid^=edit-institution-button-]`)
+        .first()
+        .click();
+    })
   cy.get('[data-testid^=button-remove-role-Institution-admin]').should('have.length', 1);
 });
 Then('they see that the Delete button on its name is disabled', () => {
