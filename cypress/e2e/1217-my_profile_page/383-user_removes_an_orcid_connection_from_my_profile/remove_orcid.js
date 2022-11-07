3d69a8e24c83293ed71bfd59abaa6d68313d43f9
siricv <siri.vikan@sikt.no>
2022-11-07 12:28:11 +0100

    Merge

--- cypress/e2e/1217-my_profile_page/383-user_removes_an_orcid_connection_from_my_profile/remove_orcid.js
+++ cypress/e2e/1217-my_profile_page/383-user_removes_an_orcid_connection_from_my_profile/remove_orcid.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-import { userRemoveOrcid } from '../../../support/constants';
-import { dataTestId } from '../../../support/dataTestIds';
-import { mockPerson } from '../../../support/mock_data';
-
-const stage = Cypress.env('STAGE') ?? 'dev';
-
-Given('user opens the page My Profile', () => {
-  cy.login(userRemoveOrcid);
-  cy.get(`[data-testid=${dataTestId.header.menuButton}]`).click({ force: true });
-  cy.get(`[data-testid=${dataTestId.header.myProfileLink}]`).click({ force: true });
-});
-When('they click Remove ORCID', () => {
-  cy.intercept(`https://api.${stage}.nva.aws.unit.no/person/*/identifiers/orcid/delete`, {
-    ...mockPerson(userRemoveOrcid),
-    orcids: [],
-  });
-  cy.get('[data-testid=button-confirm-delete-orcid]').click({ force: true });
-  cy.get('[data-testid=accept-button]').click({ force: true });
-});
-Then('they see a confirmation that the ORCID is removed', () => {
-  cy.get('[data-testid=snackbar-success]').should('be.visible');
-});
-=======
 import { userRemoveOrcid } from '../../../support/constants';
 import { dataTestId } from '../../../support/dataTestIds';
 import { mockPerson } from '../../../support/mock_data';
@@ -44,4 +20,3 @@ When('they click Remove ORCID', () => {
 Then('they see a confirmation that the ORCID is removed', () => {
   cy.get('[data-testid=snackbar-success]').should('be.visible');
 });
->>>>>>> test-deploy
