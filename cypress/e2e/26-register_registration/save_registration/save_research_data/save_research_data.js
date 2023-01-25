33ef89eae4bb95f5c0734d456bb6d68addb57ec1
Eiriknil <eirik.nilsen@unit.no>
2023-01-25 09:30:22 +0100

    Fix versioning for files

--- cypress/e2e/26-register_registration/save_registration/save_research_data/save_research_data.js
+++ cypress/e2e/26-register_registration/save_registration/save_research_data/save_research_data.js
@@ -3,70 +3,84 @@ import { dataTestId } from '../../../../support/dataTestIds';
 import { registrationFields, resourceTypeFields } from '../../../../support/save_registration';
 
 const fields = {
-    'DataManagementPlan': [
-        resourceTypeFields.publisher,
-        resourceTypeFields.relatedRegistrations,
-        resourceTypeFields.externalLink,
-    ],
-    'DataSet': [
-        resourceTypeFields.geographicDescription,
-        resourceTypeFields.relatedRegistrations,
-        resourceTypeFields.relatedDMPs,
-        resourceTypeFields.externalLink,
-    ],
+  'DataManagementPlan': [
+    resourceTypeFields.publisher,
+    resourceTypeFields.relatedRegistrations,
+    resourceTypeFields.externalLink,
+  ],
+  'DataSet': [
+    resourceTypeFields.geographicDescription,
+    resourceTypeFields.relatedRegistrations,
+    resourceTypeFields.relatedDMPs,
+    resourceTypeFields.externalLink,
+  ],
 };
 
-const commonContributorRoles = ['DataCollector', 'DataCurator', 'DataManager', 'Distributor', 'ContactPerson', 'Editor', 'RelatedPerson', 'Researcher', 'RightsHolder', 'Supervisor', 'Other'];
+const commonContributorRoles = [
+  'DataCollector',
+  'DataCurator',
+  'DataManager',
+  'Distributor',
+  'ContactPerson',
+  'Editor',
+  'RelatedPerson',
+  'Researcher',
+  'RightsHolder',
+  'Supervisor',
+  'Other',
+];
 
 const researchContributorRoles = {
-    'DataManagementPlan': ['Creator', ...commonContributorRoles],
-    'DataSet': [...commonContributorRoles],
+  'DataManagementPlan': ['Creator', ...commonContributorRoles],
+  'DataSet': [...commonContributorRoles],
 };
 
 // Scenario Outline: Creator sees registration is saved with correct values presented on landing page for Research data
 Given('Author begins registering a Registration', () => {
-    cy.login(userSaveResearchData);
-    cy.startWizardWithEmptyRegistration();
+  cy.login(userSaveResearchData);
+  cy.startWizardWithEmptyRegistration();
 });
 And('selects {string}', (resourceType) => {
-    cy.wrap(resourceType).as('resourceType');
+  cy.wrap(resourceType).as('resourceType');
 });
 And('fill in values for all fields', () => {
-    cy.get('@resourceType').then((resourceType) => {
-        cy.fillInResourceType(resourceType, fields[resourceType]);
-        cy.getDataTestId(dataTestId.registrationWizard.stepper.contributorsStepButton).click();
-        const contributorRoles = researchContributorRoles[resourceType];
-        cy.fillInContributors(contributorRoles);
-    });
-    cy.fillInCommonFields();
+  cy.get('@resourceType').then((resourceType) => {
+    cy.fillInResourceType(resourceType, fields[resourceType]);
+    cy.getDataTestId(dataTestId.registrationWizard.stepper.contributorsStepButton).click();
+    const contributorRoles = researchContributorRoles[resourceType];
+    cy.fillInContributors(contributorRoles);
+  });
+  cy.fillInCommonFields();
 });
 When('they saves Registration', () => {
-    cy.get(`[data-testid=${dataTestId.registrationWizard.stepper.filesStepButton}]`).click();
-    cy.get('[data-testid="button-save-registration"]').click();
+  cy.get(`[data-testid=${dataTestId.registrationWizard.stepper.filesStepButton}]`).click();
+  cy.get('[data-testid="button-save-registration"]').click();
 });
 Then('they can see the values on the Registration Landing Page', () => {
-    cy.get('@resourceType').then((subtype) => {
-        cy.checkLandingPage(subtype);
-    });
+  cy.get('@resourceType').then((subtype) => {
+    cy.checkLandingPage(subtype);
+  });
 });
 And('they can see the values in the Registration Wizard', () => {
-    cy.get('[data-testid=button-edit-registration]').click();
-    Object.keys(registrationFields).forEach((key) => {
-        cy.get(`[data-testid=${registrationFields[key].tab}]`).click();
-        Object.keys(registrationFields[key]).forEach((subkey) => {
-            if (subkey !== 'tab') {
-                const field = registrationFields[key][subkey];
-                cy.checkField(field);
-            }
-        });
+  cy.get('[data-testid=button-edit-registration]').click();
+  Object.keys(registrationFields).forEach((key) => {
+    cy.get(`[data-testid=${registrationFields[key].tab}]`).click();
+    Object.keys(registrationFields[key]).forEach((subkey) => {
+      if (subkey !== 'tab') {
+        const field = registrationFields[key][subkey];
+        if (subkey !== 'version') {
+          cy.checkField(field);
+        }
+      }
     });
-    cy.get('@resourceType').then((subtype) => {
-        cy.get(`[data-testid=${dataTestId.registrationWizard.stepper.resourceStepButton}]`).click();
-        fields[subtype].forEach((field) => {
-            cy.checkField(field);
-        });
-        cy.checkContributors(researchContributorRoles[subtype])
+  });
+  cy.get('@resourceType').then((subtype) => {
+    cy.get(`[data-testid=${dataTestId.registrationWizard.stepper.resourceStepButton}]`).click();
+    fields[subtype].forEach((field) => {
+      cy.checkField(field);
     });
+    cy.checkContributors(researchContributorRoles[subtype]);
+  });
 });
 
 // Examples:
