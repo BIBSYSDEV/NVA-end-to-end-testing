7e07fc117a9134ce16bb6aab94ed79cb7007ccf1
Eirik <eirik.nilsen@unit.no>
2023-03-06 10:25:48 +0100

    Fix python script

--- test_data/create_test_data.py
+++ test_data/create_test_data.py
@@ -2,7 +2,7 @@ import import_users_new
 import import_customers
 import import_publications
 
-import_customers.run()
 import_users_new.run('./users/testdata_user.json', True)
+import_customers.run()
 import_users_new.run('./users/test_users_new.json', False)
 import_publications.run()
