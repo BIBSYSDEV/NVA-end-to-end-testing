f1d9366fc4fc00d0e16374459a8603b4a5674392
Eirik Nilsen <eirik.nilsen@sikt.no>
2023-05-25 09:09:00 +0200

    Fix support message

--- cypress/e2e/26-register_registration/creator_requests_support/creator_requests_support.js
+++ cypress/e2e/26-register_registration/creator_requests_support/creator_requests_support.js
@@ -1,18 +1,20 @@
 import { userRequestSupport } from '../../../support/constants';
+import { dataTestId } from '../../../support/dataTestIds';
 
 Given('Creator begins registering a Registration in the Wizard', () => {
   cy.login(userRequestSupport);
   cy.startWizardWithEmptyRegistration();
 });
 When('they click the "Request support" button', () => {
-  cy.get('[data-testid=open-support-button]').click({ force: true });
+  cy.getDataTestId(dataTestId.registrationWizard.formActions.openSupportButton).click();
 });
 Then('the "Request support" dialog is opened', () => {
-  cy.get('[data-testid=support-modal]').should('be.visible');
+  cy.getDataTestId(dataTestId.registrationWizard.formActions.supportModal).should('be.visible');
 });
 And('they see field for Message', () => {
-  cy.get('[data-testid=message-field]').should('be.visible');
+  cy.getDataTestId(dataTestId.registrationLandingPage.tasksPanel.supportAccordion).click();
+  cy.getDataTestId(dataTestId.registrationLandingPage.tasksPanel.messageText).should('exist');
 });
 And('they see a "Send Request" button', () => {
-  cy.get('[data-testid=send-button]').should('be.visible');
+  cy.getDataTestId('send-button').should('exist');
 });
