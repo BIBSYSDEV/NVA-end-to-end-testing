dd040c9731cb8f0917c2485d995cd833910067d8
Eiriknil <eirik.nilsen@unit.no>
2023-06-23 08:08:43 +0200

    Fix doi tests

--- cypress/e2e/1228-landing_page/1242-request_draft_doi_button_is_disabled_for_publication_with_existing_doi/disabled_request_doi_button.js
+++ cypress/e2e/1228-landing_page/1242-request_draft_doi_button_is_disabled_for_publication_with_existing_doi/disabled_request_doi_button.js
@@ -16,15 +16,25 @@ Given('that a Creator views the Landing Page for a Registration', () => {
   cy.login(userDraftDoi);
   cy.openMyRegistrations();
 });
-And('they are the Owner of this Registration', () => { });
+And('they are the Owner of this Registration', () => {});
 And('the Registration has status {string}', (status) => {
   cy.wrap(status).as('status');
-  cy.get(`[data-testid=${myRegistrationsTabs[status]}]`).click();
-  cy.get('[data-testid^=registration-title]')
+  cy.getDataTestId('my-registrations-unpublished-checkbox').then((unPublishedCheckBox) => {
+    const unPublished = unPublishedCheckBox.find('.Mui-checked').length > 0;
+    cy.log(unPublished);
+    ((status === 'Draft' && !unPublished) || (status === 'Published' && unPublished)) &&
+      cy.getDataTestId('my-registrations-unpublished-checkbox').click();
+  });
+  cy.getDataTestId('my-registrations-published-checkbox').then((publishedCheckBox) => {
+    const published = publishedCheckBox.find('.Mui-checked').length > 0;
+    ((status === 'Draft' && published) || (status === 'Published' && !published)) &&
+      cy.getDataTestId('my-registrations-published-checkbox').click();
+  });
+  cy.get(`[data-testid^=${dataTestId.startPage.searchResultItem}]`)
     .filter(`:contains(${registrationTitles[status]})`)
     .parent()
     .within(() => {
-      cy.get('[data-testid^=open-registration]').first().click();
+      cy.get('p > a').first().click();
     });
 });
 And('the Registration has a DOI', () => {
@@ -36,7 +46,7 @@ When('they see the Status Bar', () => {
       cy.get(`[data-testid=${dataTestId.registrationLandingPage.tasksPanel.doiRequestAccordion}]`).should('be.visible');
       cy.get(`[data-testid=${dataTestId.registrationLandingPage.tasksPanel.doiRequestAccordion}]`).click();
     }
-  })
+  });
 });
 Then('they see that the {string} button is not visible', (button) => {
   cy.get(`[data-testid=${landingPageButtons[button]}]`).should('not.exist');
