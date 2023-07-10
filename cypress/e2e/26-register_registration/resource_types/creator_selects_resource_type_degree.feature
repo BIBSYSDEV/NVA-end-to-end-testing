8966a8336bdaa1d2b14a72b05e22cd8b0cbc3619
Eirik Nilsen <eirik.nilsen@sikt.no>
2023-07-10 09:10:38 +0200

    Exit login if not allowed to use username/password

--- cypress/e2e/26-register_registration/resource_types/creator_selects_resource_type_degree.feature
+++ cypress/e2e/26-register_registration/resource_types/creator_selects_resource_type_degree.feature
@@ -1,8 +1,6 @@
 Feature: Creator selects Resource type Degree
 
-    @TEST_NP-4062
     @test
-    @394
     Scenario: Creator navigates to the Resource Type tab and selects Resource type "Student thesis"
         Given Creator navigates to Resource Type tab
         When they select the Resource type "Student thesis"
@@ -13,9 +11,7 @@ Feature: Creator selects Resource type Degree
             | Licentiate thesis    |
             | Other student thesis |
 
-    @TEST_NP-4064
     @test
-    @1694
     Scenario Outline: Creator sees fields for Resource subtypes for "Student thesis"
         Given Creator navigates to the Resource Type tab and selects Resource type "Student thesis"
         When they select the Subtype "<Subtype>"
@@ -30,7 +26,6 @@ Feature: Creator selects Resource type Degree
             | Doctoral thesis      |
             | Other student thesis |
 
-    @TEST_NP-4063
     @test
     Scenario: Creator sees that fields are validated for Resource subtypes for "Student thesis"
         Given Creator sees fields for Resource subtypes for "Student thesis"
@@ -38,9 +33,7 @@ Feature: Creator selects Resource type Degree
         Then they can see "Mandatory" error messages for fields:
             | Search box for Publisher |
 
-    @TEST_NP-4061
     @test
-    @2776
     Scenario Outline: Creator sees series fields for Resource subtypes "Doctoral thesis" and "Licentiate thesis"
         Given Creator navigates to the Resource Type tab and selects Resource type "Student thesis"
         When they select the Subtype "<DegreeType>"
--- test_data/login_warmup.py
+++ test_data/login_warmup.py
@@ -12,31 +12,35 @@ username = 'Dataporten_c924937b-f153-4836-bb7a-401893b27ba8'
 def login_warmup():
     client = boto3.client('cognito-idp')
     password = f'P_{str(uuid.uuid4())}'
-    client.admin_set_user_password(
-        Password=password,
-        UserPoolId=USER_POOL_ID,
-        Username=username,
-        Permanent=True,
-    )
-    trying = True
-    count = 0
-    while trying:
-        try:
-            response = client.initiate_auth(
-                AuthFlow='USER_PASSWORD_AUTH',
-                ClientId=CLIENT_ID,
-                AuthParameters={
-                    'USERNAME': username,
-                    'PASSWORD': password
-                }
-            )
-            return response['AuthenticationResult']['AccessToken']
-        except:
-            print(response.status_code)
-            print(response.json())
-            count+=1
-            if count == 3: trying = False
+    try:
+        client.admin_set_user_password(
+            Password=password,
+            UserPoolId=USER_POOL_ID,
+            Username=username,
+            Permanent=True,
+        )
+        trying = True
+        count = 0
+        while trying:
+            try:
+                response = client.initiate_auth(
+                    AuthFlow='USER_PASSWORD_AUTH',
+                    ClientId=CLIENT_ID,
+                    AuthParameters={
+                        'USERNAME': username,
+                        'PASSWORD': password
+                    }
+                )
+                return response['AuthenticationResult']['AccessToken']
+            except:
+                print(response.status_code)
+                print(response.json())
+                count+=1
+                if count == 3: trying = False
+    except:
+        return ''
     return ''
+
 
 
 if __name__ == '__main__':
