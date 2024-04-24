de8756f394e693d160af639c0c7981ce23985f4e
Eirik Nilsen <eirik.nilsen@sikt.no>
2024-04-24 09:05:51 +0200

    Fix datefields

--- cypress.config.js
+++ cypress.config.js
@@ -19,6 +19,14 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.family === 'chromium' && browser.name !== 'electron') {
+          // auto open devtools
+          launchOptions.args.push('--blink-settings=primaryPointerType=4')
+        }
+        return launchOptions;
+      });
+
       return require('./cypress/plugins/index.js')(on, config);
     },
     specPattern: 'cypress/e2e/**/*.feature',
