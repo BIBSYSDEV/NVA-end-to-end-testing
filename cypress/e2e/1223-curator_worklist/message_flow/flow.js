960ea3d9a4608a45ca7a6dbe760df5ad9c2b4008
Eirik Nilsen <eirik.nilsen@sikt.no>
2024-08-26 09:55:37 +0200

    Messages

--- cypress/e2e/1223-curator_worklist/message_flow/flow.js
+++ cypress/e2e/1223-curator_worklist/message_flow/flow.js
@@ -34,8 +34,8 @@ When('they view the main page for NVA', () => {
     cy.getDataTestId(dataTestId.startPage.searchResultItem).should('be.visible');
 });
 Then('they see the number of unassigned tasks', () => {
+    cy.wait(15000);
     cy.getDataTestId(dataTestId.header.tasksLink).within(() => {
-        cy.wait(5000);
         cy.get('span > span > span').should('exist');
         cy.get('span > span > span').filter(':contains("0")').should('not.exist');
         cy.get('span > span > span').then(($it) => cy.wrap($it.text()).as('taskNumbers'));
