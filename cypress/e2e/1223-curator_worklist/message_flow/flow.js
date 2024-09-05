bb86b9e457c26a8c173f834b21cbfe4a141516ea
Eirik Nilsen <eirik.nilsen@sikt.no>
2024-09-05 11:16:01 +0200

    message flow delay = 30sec

--- cypress/e2e/1223-curator_worklist/message_flow/flow.js
+++ cypress/e2e/1223-curator_worklist/message_flow/flow.js
@@ -34,7 +34,7 @@ When('they view the main page for NVA', () => {
     cy.getDataTestId(dataTestId.startPage.searchResultItem).should('be.visible');
 });
 Then('they see the number of unassigned tasks', () => {
-    cy.wait(15000);
+    cy.wait(30000);
     cy.getDataTestId(dataTestId.header.tasksLink).within(() => {
         cy.get('span > span > span').should('exist');
         cy.get('span > span > span').filter(':contains("0")').should('not.exist');
