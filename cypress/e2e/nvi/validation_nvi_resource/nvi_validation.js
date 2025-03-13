f86e8372f13690831b32492f4f0feed4f0128239
Eirik Nilsen <eirik.nilsen@sikt.no>
2025-03-13 08:20:07 +0100

    Bugfiks

--- cypress/e2e/nvi/validation_nvi_resource/nvi_validation.js
+++ cypress/e2e/nvi/validation_nvi_resource/nvi_validation.js
@@ -65,7 +65,6 @@ Then('the Curator can see the details of the Candidate', () => {
   cy.contains('Dialogue').should('be.visible');
 });
 And('the calculated number of points for the Candidate', () => {
-  cy.getDataTestId(dataTestId.registrationLandingPage.tasksPanel.)
   cy.contains('Points')
     .parent()
     .parent()
