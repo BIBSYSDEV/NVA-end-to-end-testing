b700b95d94f884b7c0ed3d29fa21860b051ba684
Eirik Nilsen <eirik.nilsen@sikt.no>
2025-05-09 12:39:02 +0200

    NVI

--- cypress/e2e/nvi/nvi-workflow-change/nvi-workflow-change.ts
+++ cypress/e2e/nvi/nvi-workflow-change/nvi-workflow-change.ts
@@ -2,7 +2,7 @@
 
 import { userChangeNviCuratorInstitutionA, userNviInstitutionA } from '../../../support/constants';
 import { dataTestId } from '../../../support/dataTestIds';
-import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
+import { Given, When, Then, BeforeAll } from '@badeball/cypress-cucumber-preprocessor';
 import { v4 as uuid } from 'uuid';
 
 const unidentifiedContributor = 'Change User NVI-institution A TestUser';
@@ -10,13 +10,13 @@ const NVIContributor = 'Change User NVI-institution B TestUser';
 const NVAContributor = 'Change User NVA-institution C TestUser';
 const externalContributor = 'External User';
 
-const noCollaboration = 'No collaboration';
+const noCollaboration = 'no Collaboration';
 const NVICollaboration = 'NVI institution Collaboration';
 const NVACollaboration = 'NVA institution Collaboration';
 const externalCollaboration = 'external institution Collaboration';
 
 const contributors = {
-  'No collaboration': '',
+  'no Collaboration': '',
   'NVI institution Collaboration': NVIContributor,
   'NVA institution Collaboration': NVAContributor,
   'external institution Collaboration': externalContributor,
@@ -33,6 +33,7 @@ const changeToUnidentifiedUser = () => {
   cy.getDataTestId(dataTestId.registrationWizard.contributors.selectUserButton).click();
   cy.getDataTestId(dataTestId.registrationWizard.formActions.saveRegistrationButton).click();
   cy.getSuccess();
+  cy.getSuccessDone();
 };
 
 const addContributor = (contributor: string) => {
@@ -55,40 +56,77 @@ const addContributor = (contributor: string) => {
   }
   cy.getDataTestId(dataTestId.registrationWizard.formActions.saveRegistrationButton).click();
   cy.getSuccess();
+  cy.getSuccessDone();
 };
+
+const titleUnidentifiedToIdentifiedContributor = 'Change from unidentified to identified';
+const titleNonScientificToScientific = 'Change from non-scientific to scientific';
+const titleScientificToNonScientific = 'Change from scientific to non-scientific';
+const titleNonScientificToScientificUnidentifiedToIdentified =
+  'Change from non-scientific to scientific, unidentified to identified';
+const titleScientificToNonScientificUnidentfiedToIdentified =
+  'Change from scientific to non-scientific, unidentified to identified';
+
+const titleRoots = [
+  titleUnidentifiedToIdentifiedContributor,
+  titleNonScientificToScientific,
+  titleScientificToNonScientific,
+  titleNonScientificToScientificUnidentifiedToIdentified,
+  titleScientificToNonScientificUnidentfiedToIdentified,
+];
+
+const titles = {};
+
+const collaborations = [noCollaboration, NVICollaboration, NVACollaboration, externalCollaboration];
+
+BeforeAll(() => {
+  cy.login(userNviInstitutionA);
+  cy.getDataTestId(dataTestId.common.skeleton).should('not.exist');
+  titleRoots.forEach((titleRoot) => {
+    titles[titleRoot] = [];
+    collaborations.forEach((collaboration) => {
+      const title = `${titleRoot} Manual ${collaboration} ${uuid()}`;
+      titles[titleRoot][collaboration] = title;
+      const category =
+        titleRoot === titleNonScientificToScientific ||
+        titleRoot === titleNonScientificToScientificUnidentifiedToIdentified
+          ? 'JournalReview'
+          : 'AcademicArticle';
+      cy.createPublishedRegistration(title, category);
+      changeToUnidentifiedUser();
+      if (collaboration !== noCollaboration) {
+        addContributor(contributors[collaboration]);
+      }
+    });
+  });
+});
 
 // Scenario Outline: Contributor changes from unidentified to identified
 Given('a curator opens a Result that is a NVI-candidate with an unidentified contributor', () => {
-  titleRoot = 'Change from unidentified to identified';
+  titleRoot = titleUnidentifiedToIdentifiedContributor;
+  cy.wrap(titleRoot).as('titleRoot');
   cy.wrap('AcademicArticle').as('category');
 });
 Given('the Result is {string} registration', (source) => {
   cy.wrap(source).as('source');
 });
 Given('the Result is {string}', (collaboration: string) => {
-  cy.get('@source').then((source) => {
-    cy.get('@category').then((category) => {
-      const title: string = `${titleRoot} ${source} ${collaboration} ${uuid()}`;
-      cy.wrap(title).as('title');
-      cy.login(userNviInstitutionA);
-      cy.createPublishedRegistration(title, category.toString());
-      changeToUnidentifiedUser();
-      if (collaboration !== 'no Collaboration') {
-        addContributor(contributors[collaboration]);
-      }
+  cy.get('@titleRoot').then((titleRoot) => {
+    const title: string = titles[titleRoot.toString()][collaboration];
+    console.log(title);
+    cy.wrap(title).as('title');
 
-      cy.login(userChangeNviCuratorInstitutionA);
-      cy.getDataTestId(dataTestId.common.skeleton).should('not.exist');
-      cy.getDataTestId(dataTestId.startPage.searchField).type(`${title}{enter}`);
-      cy.getDataTestId(dataTestId.common.skeleton).should('not.exist');
-      cy.getDataTestId(dataTestId.startPage.searchResultItem)
-        .filter(`:contains(${title})`)
-        .first()
-        .within(() => {
-          cy.get('a').filter(`:contains(${title})`).first().click();
-        });
-      cy.getDataTestId(dataTestId.registrationLandingPage.editButton).click();
-    });
+    cy.login(userChangeNviCuratorInstitutionA);
+    cy.getDataTestId(dataTestId.common.skeleton).should('not.exist');
+    cy.getDataTestId(dataTestId.startPage.searchField).type(`${title}{enter}`);
+    cy.getDataTestId(dataTestId.common.skeleton).should('not.exist');
+    cy.getDataTestId(dataTestId.startPage.searchResultItem)
+      .filter(`:contains(${title})`)
+      .first()
+      .within(() => {
+        cy.get('a').filter(`:contains(${title})`).first().click();
+      });
+    cy.getDataTestId(dataTestId.registrationLandingPage.editButton).click();
   });
 });
 When('the curator changes a contributor from unidentified to identified', () => {
@@ -123,19 +161,15 @@ Then('the Result is a NVI-candidate', () => {
 // Examples:
 //     | Source | Collaboration                      |
 //     | Manual | no Collaboration                   |
-//     | Import | no Collaboration                   |
 //     | Manual | NVI institution Collaboration      |
-//     | Import | NVI institution Collaboration      |
 //     | Manual | NVA institution Collaboration      |
-//     | Import | NVA institution Collaboration      |
 //     | Manual | external institution Collaboration |
-//     | Import | external institution Collaboration |
 
 // Scenario Outline: Category changes from non-scientific to scientific
 Given('a curator opens a non-scientific Result that is a NVI-candidate', () => {
   cy.login(userChangeNviCuratorInstitutionA);
-  titleRoot = 'Change from non-scientific to scientific';
-  cy.wrap('JournalReview').as('category');
+  titleRoot = titleNonScientificToScientific;
+  cy.wrap(titleRoot).as('titleRoot');
 });
 When('the curator changes the Category from non-scientific to scientific', () => {
   cy.getDataTestId(dataTestId.registrationWizard.stepper.resourceStepButton).click();
@@ -147,8 +181,8 @@ When('the curator changes the Category from non-scientific to scientific', () =>
 // Scenario Outline: Category changes from scientific to non-scientific
 Given('a curator opens a scientific Result that is a NVI-candidate', () => {
   cy.login(userChangeNviCuratorInstitutionA);
-  titleRoot = 'Change from scientific to non-scientific';
-  cy.wrap('AcademicArticle').as('category');
+  titleRoot = titleScientificToNonScientific;
+  cy.wrap(titleRoot).as('titleRoot');
 });
 When('the curator changes the Category from scientific to non-scientific', () => {
   cy.getDataTestId(dataTestId.registrationWizard.stepper.resourceStepButton).click();
@@ -182,13 +216,13 @@ Then('the Result is not a NVI-candidate', () => {
 // Scenario Outline: Category changes from non-scientific to scientific, contributor changes from unidentified to identified
 Given('a curator opens a non-scientific Result that is a NVI-candidate with unidentified contributor', () => {
   cy.login(userChangeNviCuratorInstitutionA);
-  titleRoot = 'Change from non-scientific to scientific, unidentified to identified';
-  cy.wrap('JournalReview').as('category');
+  titleRoot = titleNonScientificToScientificUnidentifiedToIdentified;
+  cy.wrap(titleRoot).as('titleRoot');
 });
 
 // Scenario Outline: Category changes from scientific to non-scientific, contributor changes from unidentified to identified
 Given('a curator opens a scientific Result that is a NVI-candidate with unidentified contributor', () => {
   cy.login(userChangeNviCuratorInstitutionA);
-  titleRoot = 'Change from scientific to non-scientific, unidentified to identified';
-  cy.wrap('AcademicArticle').as('category');
+  titleRoot = titleScientificToNonScientificUnidentfiedToIdentified;
+  cy.wrap(titleRoot).as('titleRoot');
 });
