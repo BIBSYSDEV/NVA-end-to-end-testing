65457444a1fd76d8e8e1f0d852ed6744c57ecf3b
Eirik Nilsen <eirik.nilsen@sikt.no>
2025-11-20 11:24:43 +0100

    File publishing test

---
+++ cypress/e2e/files_handling/file_publishing.feature
@@ -0,0 +1,30 @@
+Feature: test for metadata and file publishing
+
+  @test
+  Scenario Outline: Verify that a publication with files can be published and is listed as NVI candidate
+    Given a creator is logged in
+    When the creator creates and publishes an AcademicArticle with with files
+    Then the file is shown in the log as published and not retracted
+
+    Examples:
+      | Count |
+      |     1 |
+      |     2 |
+      |     3 |
+      |     4 |
+      |     5 |
+      |     6 |
+      |     7 |
+      |     8 |
+      |     9 |
+      |    10 |
+      |     1 |
+      |     2 |
+      |     3 |
+      |     4 |
+      |     5 |
+      |     6 |
+      |     7 |
+      |     8 |
+      |     9 |
+      |    10 |
---
+++ cypress/e2e/files_handling/file_publishing/file_publishing.ts
@@ -0,0 +1,30 @@
+// Feature: test for metadata and file publishing
+
+import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
+import { TestUsers } from "../../../support/constants";
+import { v4 as uuid } from "uuid";
+import { dataTestId } from "../../../support/dataTestIds";
+
+    // Scenario: Verify that a publication with files can be published and is listed as NVI candidate
+        Given ('a creator is logged in', () => {
+            cy.login(TestUsers.publishing.noRights);
+        });
+        When ('the creator creates and publishes an AcademicArticle with with files', () => {
+            const title = `File publishing article ${uuid()}`;
+            cy.wrap(title).as('registrationTitle');
+            cy.createPublishedRegistration(title, 'AcademicArticle', 'example.txt');
+            cy.login(TestUsers.curators.bibsys.collaboration);
+            cy.getDataTestId(dataTestId.header.tasksLink).click();
+            cy.searchFor(title);
+            cy.contains(title).click();
+            cy.getDataTestId(dataTestId.registrationLandingPage.tasksPanel.publishingRequestAcceptButton).click();
+            cy.wait(1000);
+            cy.reload();
+            cy.getDataTestId(dataTestId.registrationLandingPage.tasksPanel.tabPanelLog).click();
+            cy.wait(1000);
+        });
+            
+        Then ('the file is shown in the log as published and not retracted', () => {
+            cy.contains('Open file published').should('exist');
+            cy.contains('File retracted').should('not.exist');
+        });
