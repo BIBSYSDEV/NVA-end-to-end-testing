4f317c65b53ac70429bb5dd1dc776e241752e069
Torbjørn Kvåle <torbjorn.kvale@sikt.no>
2026-09-03 11:22:39 +0200

    style(cypress): apply prettier formatting to nvi user test

--- cypress/e2e/nvi/user/user.ts
+++ cypress/e2e/nvi/user/user.ts
@@ -1,9 +1,9 @@
 // Feature: log in testuser
 
-import { Given } from "@badeball/cypress-cucumber-preprocessor";
-import { TestUsers } from "../../../support/constants"
+import { Given } from '@badeball/cypress-cucumber-preprocessor';
+import { TestUsers } from '../../../support/constants';
 
 // Scenario: User logs in
-Given ("a User logs in", () => {
+Given('a User logs in', () => {
   cy.login(TestUsers.curators.basicnvi);
-})
+});
